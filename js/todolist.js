c19f04b630104ea83334485a95895d5b37ed56a0
jazmincolman <41834867+jazmincolman@users.noreply.github.com>
2019-03-20 10:54:29 -0300

    Update todolist.js

--- js/todolist.js
+++ js/todolist.js
@@ -35,16 +35,16 @@
         //    error
         //  - La llamada debe ser asíncrona.
 		
-		curl --location --request GET "https://task-backend-fpuna.herokuapp.com/tasks" \
-			--header "Accept: application/json" \
-			--data {
-			\"description\":\"Terminar la tarea\"
+	curl --location --request GET "https://task-backend-fpuna.herokuapp.com/tasks" \
+		--header "Accept: application/json" \
+		--data {
+		\"description\":\"Terminar la tarea\"
 		};
 		
-		XMLHttpRequest xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = () => { console.log(xhr.readyState); console.log(xhr.status); console.log(xhr.responseText); };
-		xhr.open('GET','https://task-backend-fpuna.herokuapp.com/tasks');
-		hr.send();
+		//XMLHttpRequest xhr = new XMLHttpRequest();
+		//xhr.onreadystatechange = () => { console.log(xhr.readyState); console.log(xhr.status); console.log(xhr.responseText); };
+		//xhr.open('GET','https://task-backend-fpuna.herokuapp.com/tasks');
+		//hr.send();
 
 
     };
